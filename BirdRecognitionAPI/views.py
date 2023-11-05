e181f8853c4413c04c230c3d37501d569202e2f9
palfirobert <palfi.robert14@yahoo.com>
2023-11-05 17:20:18 +0200

    Finished recognition endpoint

--- BirdRecognitionAPI/views.py
+++ BirdRecognitionAPI/views.py
@@ -1,12 +1,20 @@
+import os
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from birdnetlib import Recording
 from birdnetlib.analyzer import Analyzer
-
-
+import base64
+import resampy
 
 @api_view(['POST'])
 def getData(request):
+    sound_data=request.body
+    # Decode the base64 encoded sound data (if it's base64 encoded)
+    sound_bytes = base64.b64decode(sound_data)
+    # Save the sound data as an MP3 file named "randunica.mp3" in the resources folder
+    file_path = os.path.join('resources/sound', 'randunica.mp3')
+    with open(file_path, 'wb') as f:
+        f.write(sound_bytes)
     analyzer = Analyzer()
     recording = Recording(
         analyzer,
