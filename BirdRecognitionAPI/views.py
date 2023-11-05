71f2dd60910ca988730960056a5022ec0210f0d3
palfirobert <palfi.robert14@yahoo.com>
2023-11-05 17:48:57 +0200

    Added requirments file

--- BirdRecognitionAPI/views.py
+++ BirdRecognitionAPI/views.py
@@ -12,13 +12,13 @@ def getData(request):
     # Decode the base64 encoded sound data (if it's base64 encoded)
     sound_bytes = base64.b64decode(sound_data)
     # Save the sound data as an MP3 file named "randunica.mp3" in the resources folder
-    file_path = os.path.join('resources/sound', 'randunica.mp3')
+    file_path = os.path.join('resources/sound', 'bird_sound.mp3')
     with open(file_path, 'wb') as f:
         f.write(sound_bytes)
     analyzer = Analyzer()
     recording = Recording(
         analyzer,
-        "resources/sound/randunica.mp3",
+        "resources/sound/bird_sound.mp3",
         min_conf=0.25,
     )
     recording.analyze()
