5ac705fe52c7a3f0e040cab273d9b9ec9c2110ee
palfirobert <palfi.robert14@yahoo.com>
2023-11-19 18:47:31 +0200

    Updated code

--- BirdRecognitionAPI/views.py
+++ BirdRecognitionAPI/views.py
@@ -4,21 +4,31 @@ from rest_framework.response import Response
 from birdnetlib import Recording
 from birdnetlib.analyzer import Analyzer
 import base64
-import resampy
+from moviepy.editor import *
+
 
 @api_view(['POST'])
 def getData(request):
-    sound_data=request.body
+    sound_data = request.body
     # Decode the base64 encoded sound data (if it's base64 encoded)
     sound_bytes = base64.b64decode(sound_data)
-    # Save the sound data as an MP3 file named "randunica.mp3" in the resources folder
-    file_path = os.path.join('resources/sound', 'bird_sound.mp3')
+
+    # Ensure the directory exists
+    sound_dir = 'BirdRecognitionAPI/resources/sound'
+    if not os.path.exists(sound_dir):
+        os.makedirs(sound_dir)
+
+    # Use an absolute path
+    file_path = os.path.abspath(os.path.join(sound_dir, 'bird_sound.3gp'))
+
     with open(file_path, 'wb') as f:
         f.write(sound_bytes)
+
+
     analyzer = Analyzer()
     recording = Recording(
         analyzer,
-        "resources/sound/bird_sound.mp3",
+        file_path,
         min_conf=0.25,
     )
     recording.analyze()
