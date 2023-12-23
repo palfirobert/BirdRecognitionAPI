e7e1d683be4888e038ee9d311f01344b0eae4e91
palfirobert <palfi.robert14@yahoo.com>
2023-12-23 21:01:16 +0200

    Grown request size 100 MB

--- BirdRecognitionAPI/settings.py
+++ BirdRecognitionAPI/settings.py
@@ -100,7 +100,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760
+DATA_UPLOAD_MAX_MEMORY_SIZE = 104857600  # 100 MB
+
 FILE_UPLOAD_HANDLERS = [
     'django.core.files.uploadhandler.TemporaryFileUploadHandler',
 ]
