498d4223a72a3bd41652fe2258586a4e16dcb2e2
palfirobert <palfi.robert14@yahoo.com>
2024-03-13 19:07:20 +0200

    added endpoint for prediction with location

--- BirdRecognitionAPI/views.py
+++ BirdRecognitionAPI/views.py
@@ -28,10 +28,47 @@ def getData(request):
     recording = Recording(
         analyzer,
         file_path,
-        # lat=45.4244,
-        # lon=24.7463,
         min_conf=0.1,
     )
     recording.analyze()
+    print(recording.detections)
+    return Response(recording.detections)
+
+@api_view(['POST'])
+def getDataWithLocation(request):
+    # Retrieve lon and lat values from the request data
+    lon = request.data.get('lon')
+    lat = request.data.get('lat')
+    sound_data = request.data.get('sound_data')
+
+    # Proceed only if all required parameters are provided
+    if lon is None or lat is None or sound_data is None:
+        return Response({'error': 'Missing required parameters'}, status=400)
+
+    # Decode the base64 encoded sound data
+    sound_bytes = base64.b64decode(sound_data)
+
+    # Ensure the directory exists
+    sound_dir = 'BirdRecognitionAPI/resources/sound'
+    if not os.path.exists(sound_dir):
+        os.makedirs(sound_dir)
+
+    # Use an absolute path
+    file_path = os.path.abspath(os.path.join(sound_dir, 'bird_sound.wav'))
+
+    with open(file_path, 'wb') as f:
+        f.write(sound_bytes)
+
+    analyzer = Analyzer()
+    recording = Recording(
+        analyzer,
+        file_path,
+        lat=float(lat),
+        lon=float(lon),
+        min_conf=0.1,
+    )
+    recording.analyze()
+    print(lon)
+    print(lat)
     print(recording.detections)
     return Response(recording.detections)
